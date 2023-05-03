d3a6505105453cabbad66fa39ad01e1b0a08d54d
wonchanjoo <wcj0911@naver.com>
2023-05-03 05:59:33 +0000

    update socket -> restAPI

--- server.py
+++ server.py
@@ -5,34 +5,42 @@ import json
 from io import BytesIO
 import base64
 import cv2
+import torch
+import numpy as np
+import requests, json
 
 from gtts import gTTS
 import speech_recognition as sr
 import playsound
 
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='/home/ubuntu/BugiCareServer_Flask/last.pt', force_reload = True)
+
 def speak(text):
-
     tts = gTTS(text=text, lang='ko')
-
     filename='ttsWAV.wav'
-
     tts.save(filename)  # 음성 파일 저장
-
     playsound.playsound(filename)   # 음성 파일 출력
 
 app = Flask(__name__)
 
 @app.route("/image", methods = ["POST"])
 def image():
-	json_data = request.json
+    img_str = request.data
+    img_bytes = img_str.decode()
+    img_decoded = base64.b64decode(img_bytes)
+    img_array = np.frombuffer(img_decoded, dtype=np.uint8)
+    print(img_array)
+    img = cv2.imdecode(img_array, cv2.IMREAD_UNCHANGED)
+    result = model(img)
+    print(result)
 
-	img = json_data['img'] # str
-	img_bytes = img.encode('ascii')
-	img_base64 = base64.b64encode(img_bytes)
+    #cv2.imwrite('received_image.jpg', img, [int(cv2.IMWRITE_JPEG_QUALITY), 90])
+    
+    temp_dict = {'text': result}
+    temp_json = json.dumps(temp_dict)
+    headers = {'Content-Type': 'application/json', 'charset': 'utf-8'}
 
-	print(img_base64)
-	print(type(img_base64))
-	return img_base64
+    return temp_json
 
 @app.route("/image2", methods = ["GET"])
 def image2():
@@ -50,11 +58,6 @@ def tts():
     print(a)	# Flask 서버로 잘 넘어왔는지 확인
     speak(a)	# tts 함수로 텍스트를 넘겨 스피커로 출력하게 함
     return f'{a}'  # 확인용 반환값. 입력한 값 그대로 반환
-    
-# tts API를 위해 host와 port 임시 변경. 나중에 AWS와 Flask 연결 유무에 따라 host 변경
-# if __name__ == "__main__":
-# 	app.debug = True
-# 	app.run(host = "127.0.0.1", port = 5001)
 
 if __name__ == "__main__":
 	app.debug = True
